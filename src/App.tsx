e911ec4d10e695b9aa62b5557dff9238052f7b9b
Kubi <kubilay.ercikti@gastro-hero.de>
2021-12-01 22:25:10 +0100

    custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
+import { User } from "./types/User";
+import { useForm } from "./useForm";
 
 function App() {
-  interface User {
-    id: number;
-    name: string;
-  }
-
   const userObject: User = {
     id: 1,
     name: "kubicodes",
   };
 
-  const [user, setUser] = useState<User>(userObject);
+  const [values, handleChange] = useForm(userObject);
 
   return (
     <div>
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1>
       <button
         // works also with callback
         // onClick={() => {
@@ -25,11 +22,13 @@ function App() {
         //   });
         // }}
 
-        onClick={() => setUser({ ...user, name: "New Name" })}
+        // onClick={() => setUser({ ...user, name: "New Name" })}
       >
         Click me
       </button>
-      <div>{user.name}</div>
+      <div>{user.name}</div> */}
+
+      <input name="username" value={values.name} onChange={handleChange} />
     </div>
   );
 }
