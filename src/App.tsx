690674415779352f81c5af37db4e07ebbfa5388e
Kubi <kubilay.ercikti@gastro-hero.de>
2021-12-01 22:07:41 +0100

    use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  interface User {
+    id: number;
+    name: string;
+  }
+
+  const userObject: User = {
+    id: 1,
+    name: "kubicodes",
+  };
+
+  const [user, setUser] = useState<User>(userObject);
+
   return (
     <div>
       <h1>Hello World</h1>
+      <button
+        // works also with callback
+        // onClick={() => {
+        //   setUser((prevState) => {
+        //     return { ...prevState, name: "New Name" };
+        //   });
+        // }}
+
+        onClick={() => setUser({ ...user, name: "New Name" })}
+      >
+        Click me
+      </button>
+      <div>{user.name}</div>
     </div>
   );
 }
