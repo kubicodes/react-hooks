216f911cc0fa5db6936555413c91ceac6539aa64
kubicodes <kubilayercikti1@gmail.com>
2021-12-03 14:16:13 +0100

    cleanup master

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,6 @@
 import React, { useState } from "react";
-import "./App.css";
-import { User } from "./types/User";
-import { useForm } from "./useForm";
-
 function App() {
-  const userObject: User = {
-    id: 1,
-    name: "kubicodes",
-  };
-
-  const [values, handleChange] = useForm(userObject);
-
-  return (
-    <div>
-      {/* <h1>Hello World</h1>
-      <button
-        // works also with callback
-        // onClick={() => {
-        //   setUser((prevState) => {
-        //     return { ...prevState, name: "New Name" };
-        //   });
-        // }}
-
-        // onClick={() => setUser({ ...user, name: "New Name" })}
-      >
-        Click me
-      </button>
-      <div>{user.name}</div> */}
-
-      <input name="username" value={values.name} onChange={handleChange} />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
